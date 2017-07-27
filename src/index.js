30bd2a84ec75a003cfc358f60fc6e6c2b41848db
Satuk Kolan <34@satuk.ch>
2017-07-27 18:10:54 +0200

    fixed the basename

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ injectTapEventPlugin();
 ReactDOM.render(
   <Provider store={store}>
     <MuiThemeProvider theme={theme}>
-      <Router basename="/e-store">
+      <Router basename="/e-store-react">
         <Switch>
           <Route exact path={"/"} component={Home}/>
           <Route exact path={"/checkout"} component={Checkout}/>
