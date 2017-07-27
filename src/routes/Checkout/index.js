d0b07cdfb45a83560ca805fc0d20331141fc6f1a
Satuk Kolan <34@satuk.ch>
2017-07-27 18:10:15 +0200

    changed information about empty cart and restyled

--- src/routes/Checkout/index.js
+++ src/routes/Checkout/index.js
@@ -3,13 +3,40 @@
  */
 import React from "react";
 import Layout from "../../components/Layout";
-import Basket from "../../containers/Basket";
 import {connect} from "react-redux";
+import Receipt from "../../containers/Receipt";
+import ShoppingBasket from "material-ui-icons/ShoppingBasket";
+import Button from "material-ui/Button";
+import Typography from "material-ui/Typography";
+import {Link} from "react-router-dom";
+
+const styles = {
+  basket: {
+    height: 300,
+    width: 300,
+  },
+  button: {
+    height: 400,
+    width: 400,
+  },
+  checkout: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+};
 
 const Checkout = ({ basket }) => {
   if ( !basket.length ) {
     return <Layout>
-      <div>checkout is empty...........</div>
+      <div style={styles.checkout}>
+        <Typography type="display1" style={{ padding: '30px 0', }}>your cart is empty..</Typography>
+        <Link to="/">
+          <Button fab color="primary" style={styles.button}>
+            <ShoppingBasket style={styles.basket}/>
+          </Button>
+        </Link>
+      </div>
     </Layout>
   }
 
