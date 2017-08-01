79d0933774e9cd5b30a6f9ce6162d1469151f4c5
Satuk Kolan <34@satuk.ch>
2017-08-01 16:01:54 +0200

    fixed adding and removing item

--- src/store/reducers/basket.js
+++ src/store/reducers/basket.js
@@ -6,27 +6,51 @@ import {ADD_BASKET_ITEM, REMOVE_BASKET, REMOVE_BASKET_ITEM} from "../constants";
 export const basket = (state = {}, action) => {
   switch ( action.type ) {
     case ADD_BASKET_ITEM:
-      const newItem = action.item;
+      let addNewItem;
 
-      if ( state[newItem.id] ) {
-        newItem.quantity++;
+      if ( state[action.item.id] ) {
+        addNewItem = { ...state[action.item.id] };
+        addNewItem.quantity++;
       } else {
-        newItem.quantity = 1;
+        addNewItem = {
+          id: action.item.id,
+          name: action.item.name,
+          price: action.item.price,
+          discount: action.item.discount,
+          img: action.item.img,
+          quantity: 1,
+        }
       }
-      return Object.assign({}, state, { [newItem.id]: newItem });
+      return {
+        ...state,
+        [action.item.id]: addNewItem,
+      };
 
     case REMOVE_BASKET_ITEM:
-      const { item } = action;
-      const newState = { ...state };
+      let deleteItem = { ...state[action.item.id] };
+      let newState = { ...state };
+      let tmp = {};
 
-      if ( item.quantity > 1 ) {
-        item.quantity--;
+      if ( deleteItem.quantity > 1 ) {
+        deleteItem.quantity--;
       } else {
-        Object.values(newState).filter(it => it.quantity !== 1);
-        return Object.assign({});
+        newState[deleteItem.id].quantity = 0;
+
+        Object.values(newState).forEach(i => {
+          if ( i.quantity !== 0 ) {
+            tmp[i.id] = i;
+          }
+        });
+        
+        return {
+          ...tmp,
+        };
       }
 
-      return Object.assign({}, state, { [item.id]: item });
+      return {
+        ...newState,
+        [action.item.id]: deleteItem,
+      };
 
     case REMOVE_BASKET:
       const newRemovedBasket = {};
@@ -37,7 +61,9 @@ export const basket = (state = {}, action) => {
           newRemovedBasket[obj] = state[obj];
         }
       });
-      return Object.assign({}, newRemovedBasket);
+      return {
+        ...newRemovedBasket
+      };
 
     default:
       return state;
